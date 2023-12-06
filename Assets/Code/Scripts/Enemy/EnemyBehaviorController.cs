f2bdc3a537ee69df721185b003a6a5824e9790c7
sswcha <seungwcha@gmail.com>
2023-12-05 22:14:55 -0800

    destroy enemy if they fall, no points

--- Assets/Code/Scripts/Enemy/EnemyBehaviorController.cs
+++ Assets/Code/Scripts/Enemy/EnemyBehaviorController.cs
@@ -134,7 +134,7 @@ namespace Code.Scripts.Enemy
             UpdateState();
             if (transform.position.y < -2)
             {
-                TakeDamage(10000);
+                Destroy(gameObject);
             }
         }
 
